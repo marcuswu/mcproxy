313219f5ea477c93c19cee3130ad4f9dd5d8650d
Marcus Wu <mwu@digitaltorque.com>
2022-11-24 14:38:05 -0500

    Add test command

--- cheat/command.go
+++ cheat/command.go
@@ -4,14 +4,33 @@ import (
 	"strings"
 
 	"github.com/rs/zerolog/log"
+	"github.com/sandertv/gophertunnel/minecraft/protocol"
 	"github.com/sandertv/gophertunnel/minecraft/protocol/packet"
 )
 
 func HandleCommand(command *packet.CommandRequest, proxy Proxy) (*packet.CommandRequest, bool) {
 	log.Info().Msgf("Got command %v", command.CommandLine)
-	if strings.HasPrefix(command.CommandLine, "/give") {
-		serverMessage := packet.Text{TextType: packet.TextTypeSystem, Message: "This command is not implemented yet"}
-		_ = proxy.ClientConn.WritePacket(&serverMessage)
+	if strings.HasPrefix(command.CommandLine, "/gdbs") {
+		// serverMessage := packet.Text{TextType: packet.TextTypeSystem, Message: "This command is not implemented yet"}
+		// _ = proxy.ClientConn.WritePacket(&serverMessage)
+		transaction := packet.InventoryTransaction{
+			Actions: []protocol.InventoryAction{
+				{
+					SourceType:    protocol.InventoryActionSourceWorld,
+					InventorySlot: 0,
+					NewItem: protocol.ItemInstance{
+						StackNetworkID: 1,
+						Stack: protocol.ItemStack{
+							BlockRuntimeID: 57,
+							Count:          64,
+							HasNetworkID:   false,
+						},
+					},
+				},
+			},
+			TransactionData: &protocol.NormalTransactionData{},
+		}
+		_ = proxy.ClientConn.WritePacket(&transaction)
 		return command, false
 	}
 
